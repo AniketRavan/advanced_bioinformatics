7d29bb52deb1c93d97415dccb88ff550a0607c51
AniketRavan <aniket.lnl@gmail.com>
2018-12-04 14:18:45 -0600

    assign_peaks.py also plots kmer distribution - unresolved bug on line 67

--- assign_peaks.py
+++ assign_peaks.py
@@ -1,6 +1,29 @@
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 file_object = open("wgEncodeAwgDnaseDukeAosmcUniPk.narrowPeak","r")
 
+def nucleotide2num(char):
+    switcher = {
+        'A': 0,
+        'C': 1,
+        'G': 2,
+	'T': 3,
+    }
+    return switcher.get(char)
+
+
+
+def kmer2index(string):
+	score = 0;
+	multiplier = 1;
+	for i in range(0,len(string)):
+		score = score + nucleotide2num(string[len(string) - i - 1])*multiplier
+		multiplier = multiplier*4;
+	return score
+	
+
+feature_length = 100
 # Read range of peaks
 peak_range = np.empty(shape=(0,2), dtype=int)
 for line in file_object:
@@ -14,16 +37,36 @@ for line in file_object:
 
 print np.shape(peak_range)
 print peak_range[3,:]
+file_object.close()
 
 # Read sequence
 file_object = open("Homo_sapiens.GRCh37.74.dna.chromosome.1.fa","r")
-sequence = np.array(file_object.read())
+#	sequence = np.array(file_object.read())
+line = file_object.readline()
+sequence = ''
+for line in file_object:
+	sequence = sequence + line.replace('\n','')
+print len(sequence)
 
 windows = np.array([])
 for i in range(0, len(peak_range)):
-	for j in range(0, peak_range[i,1] - peak_range[i,0] + 200):
-		windows = np.append(windows, sequence[peak_range[i,0] - 100 + j:peak_range[i,0] - 100 + j + 200])
+	lower_bound = int(math.ceil((peak_range[i,0] - 500)/200))
+	upper_bound = int(math.floor((peak_range[i,1] - 500)/200) + 1)
 
-	
-	
+	indices = range(lower_bound, upper_bound)
+	for x in range(lower_bound, upper_bound):
+		windows = np.append(windows, sequence[x:x+200])	
 print len(windows)
+
+# Counting k-mers
+k = 5
+kmer_array = np.array([]*(4**k))
+for i in range(0,len(windows)):
+	for j in range(0,len(windows)):
+		j
+		kmer_array[kmer2index(windows[j: j+k])] += 1
+	
+
+plt.plot(range(0, len(kmer_array)), kmer_array)
+
+
